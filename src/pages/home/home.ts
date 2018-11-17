800c6b094225218bc6480767537c653773187a77
sagar manghnani <sagarmanghnani@gmail.com>
2018-11-17 12:28:07 +0530

    routed to services page by onclick

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ServicesPage} from '../services/services';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  moveToServicePage()
+  {
+    this.navCtrl.push(ServicesPage);
   }
 
 }
