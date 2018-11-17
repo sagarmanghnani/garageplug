ac7c21d42024df9849746b35291804ce9344c278
sagar manghnani <sagarmanghnani@gmail.com>
2018-11-17 19:36:34 +0530

    created the logic for selecting the one carsize only

--- src/pages/carsize/carsize.ts
+++ src/pages/carsize/carsize.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as products from '../../cartypes.json';
 /**
  * Generated class for the CarsizePage page.
  *
@@ -15,11 +15,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CarsizePage {
 
+  carSizeList: Array<Object> = [];
+  activeCarSize:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CarsizePage');
+    this.carSizeList = products.carsize;
+    console.log(this.carSizeList);
   }
 
+  getCarSize(item)
+  {
+    if(item.hasOwnProperty('active'))
+    {
+      if(!item.active)
+      {
+        item.active = true;
+      }
+      if(this.activeCarSize)
+      {
+        if(this.activeCarSize.id != item.id)
+        {
+          this.activeCarSize.active = false;
+          this.activeCarSize = item;
+        }
+      }
+      else
+      {
+        this.activeCarSize = item;
+      }
+    }
+
+    else
+    {
+      if(this.activeCarSize)
+      {
+        if(this.activeCarSize.id != item.id)
+        {
+          this.activeCarSize['active'] = false;
+        }
+      }
+      this.activeCarSize = item;
+      this.activeCarSize['active'] = true;
+    }
+
+    console.log(this.carSizeList);
+    console.log(this.activeCarSize);
+  }
 }
